3252f765592ada595cbe38e1eb67233f76a26c4d
htmimper <htmimper@gmail.com>
2025-05-08 21:31:46 +0900

    Add files via upload

---
+++ Capsule/src/Capsule.java
@@ -0,0 +1,11 @@
+
+public class Capsule {
+    public static void main(String[] args){
+        Person taro = new Person("山田太郎", 20);
+        taro.setName("花子");
+        taro.setAge(30);//（演習⑤）
+        System.out.println(taro.getName()); //get は値を取得（演習②）（演習③）
+        System.out.println( taro.getAge()); 
+        }
+    }
+
